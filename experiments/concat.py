c9a67dbe10b875db8c23af235b1da9e2dbfb6994
upura <upura0@gmail.com>
2020-05-31 18:11:04 +0900

    concat for cnn

--- experiments/concat.py
+++ experiments/concat.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from ayniy.utils import Data
+from ayniy.preprocessing import standerize
+
 
 sub = pd.read_csv('../input/atmaCup5__sample_submission.csv')
 train = pd.read_csv('../input/train.csv')
@@ -9,47 +12,58 @@ fitting = pd.read_csv('../input/fitting.csv')
 train = pd.merge(train, fitting, on='spectrum_id', how='inner')
 test = pd.merge(test, fitting, on='spectrum_id', how='inner')
 
-train.to_csv('../input/train_fitting.csv', index=False)
-test.to_csv('../input/test_fitting.csv', index=False)
+# train.to_csv('../input/train_fitting.csv', index=False)
+# test.to_csv('../input/test_fitting.csv', index=False)
 
 ef_tr = pd.read_csv('../input/ef_tr.csv')
 ef_te = pd.read_csv('../input/ef_te.csv')
 
-train_fitting_ef = pd.concat([train, ef_tr.drop(['id'], axis=1)], axis=1)
-test_fitting_ef = pd.concat([test, ef_te.drop(['id'], axis=1)], axis=1)
+fe_id = 'fe004_top10'
 
-train_fitting_ef.to_csv('../input/train_fitting_ef.csv', index=False)
-test_fitting_ef.to_csv('../input/test_fitting_ef.csv', index=False)
+top10_tr = Data.load(f'../input/X_train_{fe_id}.pkl')
+top10_te = Data.load(f'../input/X_test_{fe_id}.pkl')
 
-spec_train = []
-for i, filename in enumerate(train['spectrum_filename'].values):
-    spec_df = pd.read_csv(f'../input/spectrum_raw/{filename}', sep='\t', header=None)
-    spec_train.append(spec_df[1])
+top10_tr, top10_te = standerize(top10_tr, top10_te, {'encode_col': top10_tr.columns})
+print(top10_tr.head())
 
-spec_tr = pd.concat(spec_train, axis=1)
-spec_tr = spec_tr.T
-spec_tr.columns = [f'spec_{i}' for i in range(spec_tr.shape[1])]
-spec_tr = spec_tr.reset_index(drop=True)
+train_fitting_ef_top10 = pd.concat([train, ef_tr.drop(['id'], axis=1), top10_tr], axis=1)
+test_fitting_ef_top10 = pd.concat([test, ef_te.drop(['id'], axis=1), top10_te], axis=1)
 
-train = pd.concat([train, spec_tr], axis=1)
+train_fitting_ef_top10.to_csv('../input/train_fitting_ef_top10.csv', index=False)
+test_fitting_ef_top10.to_csv('../input/test_fitting_ef_top10.csv', index=False)
 
-spec_test = []
-for i, filename in enumerate(test['spectrum_filename'].values):
-    spec_df = pd.read_csv(f'../input/spectrum_raw/{filename}', sep='\t', header=None)
-    if len(spec_df[1]) == 512:
-        spec_test.append(spec_df[1])
-    else:
-        spec_test.append(pd.Series([0] + list(spec_df[1].values)))
+# train_fitting_ef.to_csv('../input/train_fitting_ef.csv', index=False)
+# test_fitting_ef.to_csv('../input/test_fitting_ef.csv', index=False)
 
-spec_te = pd.concat(spec_test, axis=1)
-spec_te = spec_te.T
-spec_te.columns = [f'spec_{i}' for i in range(spec_te.shape[1])]
-spec_te = spec_te.reset_index(drop=True)
+# spec_train = []
+# for i, filename in enumerate(train['spectrum_filename'].values):
+#     spec_df = pd.read_csv(f'../input/spectrum_raw/{filename}', sep='\t', header=None)
+#     spec_train.append(spec_df[1])
 
-test = pd.concat([test, spec_te], axis=1)
+# spec_tr = pd.concat(spec_train, axis=1)
+# spec_tr = spec_tr.T
+# spec_tr.columns = [f'spec_{i}' for i in range(spec_tr.shape[1])]
+# spec_tr = spec_tr.reset_index(drop=True)
 
-train.to_csv('../input/train__fitting_spec.csv', index=False)
-test.to_csv('../input/test_fitting_spec.csv', index=False)
+# train = pd.concat([train, spec_tr], axis=1)
 
-spec_tr.to_csv('../input/spec_tr.csv', index=False)
-spec_te.to_csv('../input/spec_te.csv', index=False)
+# spec_test = []
+# for i, filename in enumerate(test['spectrum_filename'].values):
+#     spec_df = pd.read_csv(f'../input/spectrum_raw/{filename}', sep='\t', header=None)
+#     if len(spec_df[1]) == 512:
+#         spec_test.append(spec_df[1])
+#     else:
+#         spec_test.append(pd.Series([0] + list(spec_df[1].values)))
+
+# spec_te = pd.concat(spec_test, axis=1)
+# spec_te = spec_te.T
+# spec_te.columns = [f'spec_{i}' for i in range(spec_te.shape[1])]
+# spec_te = spec_te.reset_index(drop=True)
+
+# test = pd.concat([test, spec_te], axis=1)
+
+# train.to_csv('../input/train__fitting_spec.csv', index=False)
+# test.to_csv('../input/test_fitting_spec.csv', index=False)
+
+# spec_tr.to_csv('../input/spec_tr.csv', index=False)
+# spec_te.to_csv('../input/spec_te.csv', index=False)
