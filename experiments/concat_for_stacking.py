683b561dd1280f6a846273d47667a8fec00c94c4
upura <upura0@gmail.com>
2020-06-02 13:44:34 +0900

    stacking

--- experiments/concat_for_stacking.py
+++ experiments/concat_for_stacking.py
@@ -7,26 +7,73 @@ from ayniy.utils import Data
 
 def load_oof_from_run_id(run_id: str):
     oof = Data.load(f'../output/pred/{run_id}-train.pkl')
-    if run_id in ('run015'):
+    if run_id in ('run013', 'run014', 'run015'):
         oof = oof.reshape(-1, )
     return oof
 
 
 def load_pred_from_run_id(run_id: str):
     pred = Data.load(f'../output/pred/{run_id}-test.pkl')
-    if run_id in ('run015'):
+    if run_id in ('run013', 'run014', 'run015'):
         pred = pred.reshape(-1, )
     return pred
 
 
 run_ids = [
-    'run015',
+    'run062',
+    'run062',
+    'run061',
+    'run060',
+    'run059',
+    'run058',
+    'run057',
+    'run056',
+    'run055',
+    'run054',
+    'run053',
+    'run052',
+    'run051',
+    'run050',
+    'run049',
+    'run048',
+    'run048',
+    'run048',
+    'run047',
+    'run046',
+    'run045',
+    'run043',
+    'run042',
+    'run041',
+    'run040',
+    'run022',
+    'run039',
+    'run036',
+    'run035',
+    'run029',
+    'run028',
+    'run027',
+    'run026',
+    'run025',
+    'run024',
     'run023',
     'run022',
-    'run035',
-    'run036',
+    'run021',
+    'run020',
+    'run019',
+    'run018',
+    'run017',
+    'run016',
+    'run015',
+    'run014',
+    'run013',
+    'run012',
+    'run011',
+    'run010',
+    'run009',
+    'run008',
+    'run001',
 ]
-fe_name = 'stack004'
+fe_name = 'stack005'
 
 y_train = pd.read_csv('../input/train.csv')['target']
 oofs = [load_oof_from_run_id(ri) for ri in run_ids]
