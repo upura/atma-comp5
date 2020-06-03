dc2962b8a8da6034bb20604bb4d8e76cf8502f32
upura <upura0@gmail.com>
2020-06-04 00:02:12 +0900

    weighted kmat

--- experiments/weighted_averaging_team.py
+++ experiments/weighted_averaging_team.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
 from sklearn.metrics import average_precision_score
+from sklearn.model_selection import StratifiedKFold
 
 from ayniy.utils import Data
 
@@ -50,19 +51,27 @@ run_ids = [
     'run064',
     'run063',
 ]
-run_name = 'weight007'
+run_name = 'weight008'
 
-y_train = pd.read_csv('../input/train.csv')['target']
+train = pd.read_csv('../input/train.csv')
+y_train = train['target']
 data = [load_from_run_id(ri) for ri in run_ids]
 
 # kmat
-dirname = 'result_0601_02_re'
-preds = []
-for i in range(1, 6):
-    preds.append(pd.read_pickle(f'../input/{dirname}/test_fold{i}.pickle'))
-kmat_pred = np.mean(preds, axis=0)
-kmat_oof = pd.read_csv('../input/kmat/result_concat.csv')['target']
-data.append((kmat_oof, kmat_pred))
+dirname = 'result_0603_01_upf0'
+kmat_preds = []
+kmat_oofs = []
+for i in range(5):
+    kmat_preds.append(pd.read_pickle(f'../input/kmat/{dirname}/test_fold{i}.pickle'))
+    kmat_oofs.append(pd.read_pickle(f'../input/kmat/{dirname}/val_fold{i}.pickle'))
+kmat_pred = np.mean(kmat_preds, axis=0)
+
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=7)
+train['oof'] = np.nan
+for i, (tr_idx, val_idx) in enumerate(cv.split(train, train['target'])):
+    train.loc[val_idx, 'oof'] = kmat_oofs[i]
+
+data.append((train['oof'].values, kmat_pred))
 
 # print cv score
 for d in data:
